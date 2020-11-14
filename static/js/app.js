de77ac508a715ba36c2c42cd35e0d9cc29e97911
Arica Jackson <aricajackson2020@gmail.com>
2020-11-14 14:13:42 -0500

    build table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// view data
+console.log(tableData);
+
+// get a reference to table body tag
+var tbody = d3.select("tbody");
+
+// build table(row,data,)
+// add tr
+tableData.forEach((ufo) => {
+    var row = tbody.append("tr");
+    // only using value not key
+    Object.entries(ufo).forEach(([key, value]) => {
+        // add td
+      var cell = row.append("td");
+    //   add values
+      cell.text(value);
+    });
+  });
+  
